85ce3e0af332a13c55cf9b8d97e4ac9ef6bbaba7
aleksawsea <alexandrapynzar57@gmail.com>
2024-12-07 17:49:24 +0400

    feat: solve 5 tasks

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -108,8 +108,12 @@ function getLinearEquationRoot(a, b) {
  *   (0,-1) (1,0)    => π/2
  *   (0,1) (0,1)     => 0
  */
-function getAngleBetweenVectors(/* x1, y1, x2, y2 */) {
-  throw new Error('Not implemented');
+function getAngleBetweenVectors(x1, y1, x2, y2) {
+  const scalarProduct = x1 * x2 + y1 * y2;
+  const vectorModulus1 = Math.sqrt(x1 ** 2 + y1 ** 2);
+  const vectorModulus2 = Math.sqrt(x2 ** 2 + y2 ** 2);
+  const angleCosine = scalarProduct / (vectorModulus1 * vectorModulus2);
+  return Math.acos(angleCosine);
 }
 
 /**
@@ -125,8 +129,8 @@ function getAngleBetweenVectors(/* x1, y1, x2, y2 */) {
  *     5     => 5
  *     0     => 0
  */
-function getLastDigit(/* value */) {
-  throw new Error('Not implemented');
+function getLastDigit(value) {
+  return value % 10;
 }
 
 /**
@@ -140,8 +144,8 @@ function getLastDigit(/* value */) {
  *     '37'     => 37
  * '-525.5'     => -525.5
  */
-function parseNumberFromString(/* value */) {
-  throw new Error('Not implemented');
+function parseNumberFromString(value) {
+  return Number(value);
 }
 
 /**
@@ -157,8 +161,8 @@ function parseNumberFromString(/* value */) {
  *   3,3,3   => 5.196152422706632
  *   1,2,3   => 3.741657386773941
  */
-function getParallelepipedDiagonal(/* a, b, c */) {
-  throw new Error('Not implemented');
+function getParallelepipedDiagonal(a, b, c) {
+  return Math.sqrt(a ** 2 + b ** 2 + c ** 2);
 }
 
 /**
@@ -199,8 +203,18 @@ function roundToPowerOfTen(/* num, pow */) {
  *   16 => false
  *   17 => true
  */
-function isPrime(/* n */) {
-  throw new Error('Not implemented');
+function isPrime(n) {
+  let divisor = 2;
+  while (divisor <= Math.sqrt(n)) {
+    if (n % divisor === 0) {
+      return false;
+    }
+    divisor += 1;
+  }
+  if (n < 2) {
+    return true;
+  }
+  return true;
 }
 
 /**
